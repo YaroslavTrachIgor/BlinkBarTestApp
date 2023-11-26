1a89bd300612d96990f309af102a705d75069b39
Yaroslav Trach <zhbr282@gmail.com>
2023-11-25 20:09:31 -0500

    Initial Commit

---
+++ BlinkBarTestApp/BlinkBarTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkBarTestAppApp.swift
+//  BlinkBarTestApp
+//
+//  Created by User on 2023-11-25.
+//
+
+import SwiftUI
+
+@main
+struct BlinkBarTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
